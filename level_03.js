30966834c46247a61c7424c611612dcfb4f73b79
kafka <kafka0238@gmail.com>
2020-01-02 13:39:30 +0300

    stack call with recursion

--- level_03.js
+++ level_03.js
@@ -9,9 +9,29 @@ function countdown(i) {
     if (i > 1)
         countdown(i - 1);
 }
+console.log('countdown');
 countdown(3);
 /*
 END
 page 64
 countdown with recursion
+ */
+
+/*
+page 69
+stack call with recursion
+ */
+function fact(x) {
+    if (x === 1)
+        return 1;
+    else
+        return x * fact(x - 1);
+}
+console.log('stack call with recursion');
+console.log(fact(3));
+/*
+END
+page 69
+stack call with recursion
+need return 6;
  */
