47188c174d7d0b3fe0dd90f61ccb014b07af78dd
kafka <kafka0238@gmail.com>
2020-01-02 13:34:58 +0300

    countdown with recursion

---
+++ level_03.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/*
+page 64
+countdown with recursion
+ */
+function countdown(i) {
+    console.log(i);
+    if (i > 1)
+        countdown(i - 1);
+}
+countdown(3);
+/*
+END
+page 64
+countdown with recursion
+ */
